af4571a6d65d4894dd0e804bdf3d7e7624fe5e5a
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-18 10:39:20 -0700

    commit tree 17cacaab0356fde39f9b65c391ef4d268bedecc5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+// // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+    
+//   });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  theDomHasLoaded();
+});
+
+  function theDomHasLoaded() {
+      let str = document.getElementById('text')
+      str.innerHTML = "This is really cool!";
+  }
+
+// 1. set up DOMContentLoaded event listener 
+// 2. User event's callback function to target the p element with id = "text"
+// - replace text with "This is really cool!"
